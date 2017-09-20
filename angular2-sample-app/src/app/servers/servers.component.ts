8dc79e49b39e68ff9f92ead7411748a807cd9f6a
Jyasveer Gotta <jyasveer@gmail.com>
2017-09-20 10:27:36 +0530

    Class: 20-sep-2017

--- angular2-sample-app/src/app/servers/servers.component.ts
+++ angular2-sample-app/src/app/servers/servers.component.ts
@@ -10,9 +10,13 @@ export class ServersComponent implements OnInit {
 
   canAddServer: boolean;
   serverName: string;
+  isServerAdded: boolean;
+  servers: string[] = [];
 
   constructor() {
+    this.serverName = 'server 1';
     this.canAddServer = false;
+    this.isServerAdded = false;
     setTimeout(() => {
       this.canAddServer = true;
     }, 2000);
@@ -24,6 +28,11 @@ export class ServersComponent implements OnInit {
   onServerNameChange(event) {
     console.log(event);
     this.serverName = event.target.value;
+  }
+
+  addServer() {
+    this.isServerAdded = true;
+    this.servers.push(this.serverName);
   }
 
 }
